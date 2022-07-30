fa19bd9e9edcc75156f77bff833d403c8eee66d0
solfire1994 <solfire1994@gmail.com>
2022-07-30 23:40:59 +0300

    Add project files.

---
+++ TaskThreePartFive/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TaskThreePartFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давай сыграем в игру Угадай число, введи верхнюю границу для игры");
+            int upperBound = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            Console.Clear();
+            Console.WriteLine("Игра началась, попробуй угадать");
+            int number = random.Next(0, upperBound + 1);
+            int userNumber;
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "")
+                {
+                    Console.WriteLine("Вы рано сдались, загаданное число {0}", number);
+                    break;
+                }
+                else
+                {
+                    userNumber = int.Parse(str);
+                    if (userNumber < number)
+                    {
+                        Console.WriteLine("Загаданное число больше");
+                    }
+                    else if (userNumber > number)
+                    {
+                        Console.WriteLine("Загаданное число меньше");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы угадали, загаданное число {0}", number);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
